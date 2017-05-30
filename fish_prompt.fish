03173568d51c466f7157e0535ca2224805ed5f62
Patrick OConnor <pjo2110@columbia.edu>
2017-05-30 04:46:13 -0400

    update colors

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -41,15 +41,15 @@ function __simple_ass_prompt_git -d "Display the actual git branch"
     set_color purple
 
     set -l git_branch (command git symbolic-ref --quiet --short HEAD 2> /dev/null; or git rev-parse --short HEAD 2> /dev/null; or echo -n '(unknown)')
-
-    printf '%s ' $git_branch
-
-    set state (__simple_ass_prompt_parse_git_branch_state)
     if git_is_touched
       set_color f48d8d
     else
       set_color 47dfb6
     end
+    printf '%s ' $git_branch
+
+    set state (__simple_ass_prompt_parse_git_branch_state)
+
     printf '[%s]' $state
 
     set_color normal
